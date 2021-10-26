c8482175e780df34196f49c7dbee0b2ddddfc310
Mount2020 <76730136+Mount2020@users.noreply.github.com>
2021-10-26 13:12:58 +0800

    調整menu

--- src/app/menu-list/menu-list.module.ts
+++ src/app/menu-list/menu-list.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MenuListComponent } from './menu-list.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { MaterialModule } from '../material/material.module';
-
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NgZorroAntdModule } from './../ngzorro/ng-zorro-antd.module';
 
 @NgModule({
   declarations: [MenuListComponent],
@@ -11,7 +14,11 @@ import { MaterialModule } from '../material/material.module';
   imports: [
     CommonModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzCardModule,
+    NzAvatarModule,
+    NzIconModule,
+    NgZorroAntdModule
   ]
 })
 export class MenuListModule { }
