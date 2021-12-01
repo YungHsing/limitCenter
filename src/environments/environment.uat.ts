f468b22ceea356d53dd32825653ec73c5c6608c0
YungHsing <x70343@gmail.com>
2021-12-01 12:35:07 +0800

    修改環境變數url

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://my-prod-url',
-  allowOrigin: 'https://tvstg-eln-was01.lbtwsys.com:18443/limit'
+  allowOrigin: 'https://tvuat-eln-was01.lbtwsys.com:18443/limit'
 };
