805dc315a0b4426e4400d26ffe1e1571cd611c88
Mount2020 <76730136+Mount2020@users.noreply.github.com>
2022-01-27 18:04:18 +0800

    adjust env

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'http://my-prod-url',
-  allowOrigin: 'https://tvuat-eln-was01.lbtwsys.com:18443/limit',
+  allowOrigin: 'https://lb-tvuat-eln-was-18443.lbtwsys.com:18443/limit',
   from: 'uat'
 };
